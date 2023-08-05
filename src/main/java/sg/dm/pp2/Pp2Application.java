ad1694109f8844fa5c464b3af391f6769c1eb2cd
hongsc0421 <hongsc0421@gmail.com>
2023-08-05 17:33:13 +0900

    first commit

---
+++ src/main/java/sg/dm/pp2/Pp2Application.java
@@ -0,0 +1,13 @@
+package sg.dm.pp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp2Application.class, args);
+	}
+
+}
