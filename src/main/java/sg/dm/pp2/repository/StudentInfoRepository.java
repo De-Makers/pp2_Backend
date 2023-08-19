b82901241f842306596a000ebc2a215d7436ef94
hongsc0421 <hongsc0421@gmail.com>
2023-08-19 22:15:18 +0900

    Revert "학교 이메일 도메인 받아오기 API"
    This reverts commit 6a0065d056bcd125289bbcd24d4dff24d70e56ea.

--- src/main/java/sg/dm/pp2/repository/StudentInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package sg.dm.pp2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sg.dm.pp2.entity.StudentInfo;
-
-public interface StudentInfoRepository extends JpaRepository<StudentInfo, Integer> {
-
-}
--- src/main/java/sg/dm/pp2/service/email/EmailAuthCodeService.java
+++
@@ -1,7 +0,0 @@
-package sg.dm.pp2.service.email;
-
-import sg.dm.pp2.controller.dto.EmailAuthCodeCommandDTO;
-
-public interface EmailAuthCodeService {
-    void sendMail(EmailAuthCodeCommandDTO emailAuthCodeCommandDTO);
-}
