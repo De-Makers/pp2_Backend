e59b22730bba36d4531fa395ec4292a1d107f2d6
SF93 <kdman98@naver.com>
2023-08-29 18:50:02 +0900

    엔드포인트 변경

--- src/main/java/sg/dm/pp2/chatServer/ChatRoomController.java
+++ src/main/java/sg/dm/pp2/chatServer/ChatRoomController.java
@@ -3,13 +3,14 @@ package sg.dm.pp2.chatServer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatRoomController {
     @PostMapping("/chat/room")
-    public ChatRoomDTO createRoom(String roomName){
+    public ChatRoomDTO createRoom(@RequestParam("room_name") String roomName){
         return ChatRoomDTO.create(roomName);
     }
 }
